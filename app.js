8f8f0f823f2a47a81e3eadbbae550a6bc5c66e1f
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-05-30 15:06:02 +0200

    Fixed flashing background color change

--- app.js
+++ app.js
@@ -37,22 +37,23 @@ const leaveAnimation = (current, done) => {
   );
 };
 
-const enterAnimation = (current, done) => {
+const enterAnimation = (current, done, gradient) => {
   const product = current.querySelector(".image-container");
   const text = current.querySelector(".showcase-text");
   const circles = current.querySelectorAll(".circle");
   const arrow = current.querySelector(".showcase-arrow");
 
   return (
-    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
-    tlLeave.fromTo(
+    tlEnter.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlEnter.fromTo(
       product,
       { y: -100, opacity: 0 },
       { y: 0, opacity: 1, onComplete: done },
       "<"
     ),
-    tlLeave.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<"),
-    tlLeave.fromTo(
+    tlEnter.to("body", { background: gradient }, "<"),
+    tlEnter.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<"),
+    tlEnter.fromTo(
       circles,
       { y: -200, opacity: 0 },
       {
@@ -77,7 +78,16 @@ barba.init({
     //Transition entre les produits du showcase.
     {
       name: "default",
-
+      /* Avec la fonction once on a la possibilité de run le code dedans 1 fois au refresh de la page, 
+      cela nous permet de fixé le problème de gradient qui reviens au standard au refresh car on set
+      un background standard.  */
+      once(data) {
+        const done = this.async;
+        let next = data.next.container;
+        let gradient = getGradient(data.next.namespace);
+        gsap.set("body", { background: gradient });
+        enterAnimation(next, done, gradient);
+      },
       leave(data) {
         /* Quand on appuis sur la flèche pour faire suivant on a accès à un object qui contient
           à la fois notre page courrante mais aussi la page vers laquelle on se dirige. */
@@ -103,9 +113,21 @@ barba.init({
           { opacity: 0 },
           { opacity: 1, duration: 1, onComplete: done }
         ); */
-
-        enterAnimation(next, done);
+        let gradient = getGradient(data.next.namespace);
+        enterAnimation(next, done, gradient);
       },
     },
   ],
 });
+
+// Changing gradient on showcase
+function getGradient(name) {
+  switch (name) {
+    case "handbag":
+      return "linear-gradient(260deg, #b75d62, #754d4f";
+    case "boot":
+      return "linear-gradient(260deg, #5d8cb7, #4c4f70";
+    case "hat":
+      return "linear-gradient(260deg, #b27a5c, #7f5450";
+  }
+}
